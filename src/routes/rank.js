fc95f6ab975e042cff68278878654396ce48ee15
mimgggg4444 <minnki19@gmail.com>
2025-12-09 17:06:37 +0900

    fix: 불필요 코드 없앰

--- src/routes/rank.js
+++ src/routes/rank.js
@@ -24,20 +24,5 @@ export class Rank {
     );
   }
 
-  // JSON 변환 (안전한 형태 유지 위한)
-  toJSON() {
-    const info = {
-      participantId: this.participantId, // 이미 string,.,,
-      nickname: this.nickname,
-      totalScore: this.totalScore,
-      rank: this.rank,
-    };
 
-    return info;
-  }
-
-  // 문자열 디버깅용 불필?
-  toString() {
-    return `Rank(participantId=${this.participantId}, nickname=${this.nickname}, totalScore=${this.totalScore}, rank=${this.rank})`;
-  }
 }
