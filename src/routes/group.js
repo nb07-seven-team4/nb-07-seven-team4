7efbb5e26d1bdff173098d7050d22d4bd01ae303
hadongwoo02 <hadongwoo02@gmail.com>
2025-12-09 08:15:43 +0000

    group.js class 생성

---
+++ src/routes/group.js
@@ -0,0 +1,37 @@
+export class Group {
+  constructor (id, name, description, nidckname, password, image, tags, targetCount, discordWebhookUrl, discordInviteUrl, createdAt, updatedAt){
+      this.id = id; 
+      this.name = name; 
+      this.description = description; 
+      this.nickname = nidckname;
+      this.password = password; 
+      this.image = image;
+      this.tags = tags; 
+      this.targetCount = targetCount; 
+      this.discordWebhookUrl = discordWebhookUrl; 
+      this.discordInviteUrl = discordInviteUrl;
+      this.createdAt = createdAt;
+      this.updatedAt = updatedAt;
+    }
+  
+    static fromEntity({ id, name, description, nidckname, password, image, tags, targetCount, discordWebhookUrl, discordInviteUrl, createdAt, updatedAt }) {
+    const info = {
+      id: id.toString(),
+      name, 
+      description, 
+      nidckname, 
+      password, 
+      image, 
+      tags, 
+      targetCount, 
+      discordWebhookUrl, 
+      discordInviteUrl, 
+      createdAt, 
+      updatedAt
+    };
+    validateArticleInfo(info);
+    // 출입국 심사... imigration입니다...
+
+    return new Group( info.id, info.name, info.description, info.nidckname, info.password, info.image, info.tags, info.targetCount, info.discordWebhookUrl, info.discordInviteUrl, info.createdAt, info.updatedAt);
+  }
+}
