6dcc66b3872ea13dcc9eb915b6ecbbf6f08bee15
mimgggg4444 <minnki19@gmail.com>
2025-12-09 16:14:56 +0900

    feat:rank.js 기본 class 구현

---
+++ src/routes/rank.js
@@ -0,0 +1,31 @@
+// src/models/rank.js
+export class Rank {
+  constructor(participantId, nickname, totalScore, rank) {
+    this.participantId = participantId;
+    this.nickname = nickname;
+    this.totalScore = totalScore;
+    this.rank = rank;
+  }
+
+  // Prisma 데이터 + 점수 정리 후 Rank 객체로 변환
+  static fromParticipantEntity(entity, totalScore, rank) {
+    return new Rank(
+      entity.id,
+      entity.nickname,
+      totalScore,
+      rank
+    );
+  }
+
+  // JSON 응답 포맷
+  toJSON() {
+    return {
+      participantId: this.participantId,
+      nickname: this.nickname,
+      totalScore: this.totalScore,
+      rank: this.rank,
+    };
+  }
+
+  //유효성- 계산결과값이라서 유효성 검사 불필요?
+}
