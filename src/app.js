490320351258efaa52ca341990ae38193d2b37f7
mimgggg4444 <minnki19@gmail.com>
2025-12-11 16:18:11 +0900

    feat: group, record 테스트 위해 주석 처리했던 파일 주석 해제하였습ㄴ디ㅏ

--- src/app.js
+++ src/app.js
@@ -5,9 +5,9 @@ dotenv.config();
 import cors from "cors";
 import multer from "multer";
 
-// import groupRoutes from "./routes/groupRoutes.js";
+import groupRoutes from "./routes/groupRoutes.js";
 // import participantRoutes from "./routes/participantRoutes.js";
-// import recordRoutes from "./routes/recordRoutes.js";
+import recordRoutes from "./routes/recordRoutes.js";
 import rankRoutes from "./routes/rankRoutes.js";
 import imageRoutes from "./routes/imageRoutes.js";
 
@@ -27,9 +27,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
 
 // API Routes
-// app.use("/groups", groupRoutes);
+app.use("/groups", groupRoutes);
 // app.use("/groups/:groupId/participants", participantRoutes);
-// app.use("/groups/:groupId/records", recordRoutes);
+app.use("/groups/:groupId/records", recordRoutes);
 /**
  * rank및 img기능확인 위한 위의 30,31,32 주석화 하였습니다.
 */
